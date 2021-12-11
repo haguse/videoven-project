e293eb319ce0f0638d5be3e0bdc329deb499dfd3
Halit Guven Serin <halitguvens@gmail.com>
2021-12-11 13:50:19 +0300

    Create AppealSection structure

---
+++ src/components/AppealSection/PhoneContent/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PhoneContent = () => {
+  return <div></div>;
+};
+
+export default PhoneContent;
---
+++ src/components/AppealSection/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AppealSection = () => {
+  return <div></div>;
+};
+
+export default AppealSection;
